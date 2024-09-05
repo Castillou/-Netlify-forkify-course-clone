363bff6536d6355e53d8e697b14b9a56f82fe16f
Castillou <joajoa70584@gmail.com>
2024-09-05 14:14:16 +0900

    First commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '43ba87ea-8471-45c6-946c-f3aa683ba5f0';
+export const MODAL_CLOSE_SEC = 2.5;
